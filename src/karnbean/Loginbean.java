15083a3b5f566d00a7ab40c914f3f34c6e0fcf97
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2020-12-01 12:59:34 +0530

    Create Loginbean.java

---
+++ src/karnbean/Loginbean.java
@@ -0,0 +1,17 @@
+package karnbean;
+public class {
+private String uname;
+private String password;
+public String getUname() {
+return uname;
+}
+public void setUname(String uname) {
+this.uname = uname;
+}
+public String getPassword() {
+return password;
+}
+public void setPassword(String password) {
+this.password = password;
+}
+}
